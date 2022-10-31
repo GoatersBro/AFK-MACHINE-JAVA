51bde54e2d3768fe3696faa278cf952ed995e19c
GoatersBro <lliamdel@gmail.com>
2022-10-31 16:36:38 -0600

    Keypress Test
    This code allows for the computer to press down on F after 2 seconds.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+/* 
+ * This will run a loop that will click through random keys
+ * to trick the game into thinking im not AFK
+*/
+
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello NYC");
+    public static void main(String[] args) throws IOException, AWTException, InterruptedException{
+        System.out.println("Program Started");
+        Robot robot = new Robot();
+
+        Thread.sleep(2000);
+        System.out.println("Pressing Key");
+
+        robot.keyPress(KeyEvent.VK_F);
     }
 }
